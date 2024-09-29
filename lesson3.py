f887600ba8da8a7c1872e31b92432c20c3c67dd7
MichailDavidov <davidov.michail@gmail.com>
2024-09-29 21:26:48 +0400

    Практическое задание по уроку "Базовые структуры данных"

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+print('1st program')
+print(9**0.5 * 5)
+print('2st program')
+print(9.99 > 9.98 and 1000!= 1000.1)
+print('3st program')
+print(2*2+2, 2*(2+2))
+print('4st program')
+x = float('123.456')
+x = x*10
+y = x%10
+y = int(y)
+print(y)
